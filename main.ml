71c095bced6636295aa12566887e71c5ac882d06
Mikkel <mtkongsted@gmail.com>
2023-03-15 15:02:31 +0100

    made match statement

--- main.ml
+++ main.ml
@@ -10,13 +10,17 @@ edit [id]"
 let getargs inp = Str.split (Str.regexp " ") inp
 
 let rec todo () =
-    let () = print_string "\nWelcome to the todo list: " in
-      let inp = read_line() in
-        let args = getargs inp in
-          let command = List.nth args 0 in
-            if command = "help" then
-              let () = print_string help in todo ()
-            else if command = "create" then
-              todo ();;
-todo ()
+  let () = print_string "\nWelcome to the todo list: " in
+    let inp = read_line() in
+      let args = getargs inp in
+        let command = List.nth args 0 in
+            match command with
+            | "help" -> print_string help; todo ()
+            | "create" -> todo ()
+            | "delete" -> todo ()
+            | "list" -> todo ()
+            | "read" -> todo ()
+            | "edit" -> todo ()
+            | _ -> let () = print_string "Could find command, type 'help' for all commands" in todo ();;
 
+todo ()
