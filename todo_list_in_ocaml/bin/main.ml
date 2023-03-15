43301d195e87d15056466629142f69fdf550a892
Mikkel <mtkongsted@gmail.com>
2023-03-15 13:30:29 +0100

    basic get input

--- todo_list_in_ocaml/bin/main.ml
+++ todo_list_in_ocaml/bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "\nHello, World!"
+let help = ""
+
+let rec todo repeat =
+  if not repeat then
+    () 
+  else 
+    let () = print_string "\n Welcome to the todo list: " in
+      let inp = read_line() in
+        if inp = "help" then
+          let () = print_string help in todo repeat;;
+
+todo true
+       
+
+
