81460a7469cf182feecbefcc46343324c2b0c803
ReimarPB <reimarpb@gmail.com>
2023-03-15 14:40:15 +0100

    Replace first argument with unit

--- main.ml
+++ main.ml
@@ -9,19 +9,14 @@ edit [id]"
 
 let getargs inp = Str.split (Str.regexp " ") inp
 
-let rec todo repeat =
-  if not repeat then
-    () 
-  else 
+let rec todo () =
     let () = print_string "\nWelcome to the todo list: " in
       let inp = read_line() in
         let args = getargs inp in
           let command = List.nth args 0 in
             if command = "help" then
-              let () = print_string help in todo repeat
+              let () = print_string help in todo ()
             else if command = "create" then
-              todo repeat;;
-todo true
-       
-
+              todo ();;
+todo ()
 
