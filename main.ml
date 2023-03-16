50439ca2fbd321c867c4e13dfef3a56ca719b119
ReimarPB <reimarpb@gmail.com>
2023-03-16 16:17:04 +0100

    Fix exiting and add editing

--- main.ml
+++ main.ml
@@ -4,7 +4,7 @@ create [title] [content]
 delete [title]
 list
 read [title]
-edit [title]"
+edit [title] [content]"
 
 let extracted_args inp = Str.split (Str.regexp " ") inp
 
@@ -23,20 +23,18 @@ let rec todo () =
               let oc = open_out (Printf.sprintf "notes/%s.txt" (List.nth args 1)) in
                 Printf.fprintf oc "%s\n" (List.nth (Str.bounded_split (Str.regexp " ") inp 3) 2);
                 close_out oc;
-                print_string "File created";
-                todo ()
+                print_string "File created"
             else
               print_string "File already exists";
-              todo ()
+            todo ()
           | "delete" ->
             let filename = (Printf.sprintf "notes/%s.txt" (List.nth args 1)) in
               if Sys.file_exists filename then
                 let () = Sys.remove filename in
-                print_string "File deleted";
-                todo ()
+                print_string "File deleted"
               else
                 print_string "File doesn't exist";
-                todo ()
+              todo ()
           | "list" ->
             let files = Sys.readdir "notes" in
               let notes = List.filter (fun file -> Str.last_chars file 4 = ".txt") (Array.to_list files) in
@@ -50,7 +48,16 @@ let rec todo () =
               flush stdout;
               close_in_noerr ic;
               todo ()
-          | "edit" -> todo ()
+          | "edit" ->
+            let filename = (Printf.sprintf "notes/%s.txt" (List.nth args 1)) in
+            if Sys.file_exists filename then
+              let oc = open_out (Printf.sprintf "notes/%s.txt" (List.nth args 1)) in
+                Printf.fprintf oc "%s\n" (List.nth (Str.bounded_split (Str.regexp " ") inp 3) 2);
+                close_out oc;
+                print_string "File updated"
+            else
+              print_string "File doesn't exist";
+            todo ()
           | "quit" | "exit" -> ()
           | _ ->
               print_string "Could find command, type 'help' for all commands";
