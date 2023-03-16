214fef8a8db36d5aa9f419ed1e90434e3bb43739
ReimarPB <reimarpb@gmail.com>
2023-03-16 10:17:11 +0100

    Use semicolons, add quit/exit command

--- main.ml
+++ main.ml
@@ -10,17 +10,20 @@ edit [id]"
 let getargs inp = Str.split (Str.regexp " ") inp
 
 let rec todo () =
-  let () = print_string "\nWelcome to the todo list: " in
-    let inp = read_line() in
-      let args = getargs inp in
-        let command = List.nth args 0 in
-            match command with
-            | "help" -> print_string help; todo ()
-            | "create" -> todo ()
-            | "delete" -> todo ()
-            | "list" -> todo ()
-            | "read" -> todo ()
-            | "edit" -> todo ()
-            | _ -> let () = print_string "Could find command, type 'help' for all commands" in todo ();;
+  print_string "\nWelcome to the todo list: ";
+  let inp = read_line() in
+    let args = getargs inp in
+      let command = List.nth args 0 in
+          match command with
+          | "help" -> print_string help; todo ()
+          | "create" -> todo ()
+          | "delete" -> todo ()
+          | "list" -> todo ()
+          | "read" -> todo ()
+          | "edit" -> todo ()
+          | "quit" | "exit" -> ()
+          | _ ->
+              print_string "Could find command, type 'help' for all commands";
+              todo ();;
 
 todo ()
