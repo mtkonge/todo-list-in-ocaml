ad88aa2edaba3aec72422fd139bd4065f00bd88a
Mikkel <mtkongsted@gmail.com>
2023-03-16 13:31:58 +0100

    create note

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all:
+	mkdir -p notes
 	ocamlc -I +str str.cma main.ml
--- main.ml
+++ main.ml
@@ -7,16 +7,19 @@ list
 read [id]
 edit [id]"
 
-let getargs inp = Str.split (Str.regexp " ") inp
+let extracted_args inp = Str.split (Str.regexp " ") inp
 
 let rec todo () =
   print_string "\nWelcome to the todo list: ";
   let inp = read_line() in
-    let args = getargs inp in
+    let args = extracted_args inp in
       let command = List.nth args 0 in
           match command with
           | "help" -> print_string help; todo ()
-          | "create" -> todo ()
+          | "create" -> 
+            let oc = open_out (Printf.sprintf "notes/%s.txt" (List.nth args 1))  in
+            Printf.fprintf oc "%s\n" (List.nth args 2);
+            close_out oc;
           | "delete" -> todo ()
           | "list" -> todo ()
           | "read" -> todo ()
